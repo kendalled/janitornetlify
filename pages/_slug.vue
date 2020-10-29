884b623bbd31cf561a9e20e64a70ab8a5888e805
Kendall Jackson <kendallkj00@gmail.com>
2020-10-28 22:52:24 -0400

    Fix _slug asyncData

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -1,34 +1,45 @@
 <template>
   <div class="mx-auto prose-sm prose sm:prose lg:prose-lg xl:prose-2xl">
-    <nuxt-content :document="article" />
+    <nuxt-content :document="page" />
   </div>
 </template>
 
 <script>
 export default {
+  // async asyncData ({ $content, params, error }) {
+  //   const slug = params.slug || 'index'
+  //   const article = await $content(slug)
+  //     .fetch()
+  //     .catch((err) => {
+  //       error({ statusCode: 404, message: 'Page not found' + err })
+  //     })
+
+  //   return {
+  //     article
+  //   }
+  // },
   async asyncData ({ $content, params, error }) {
     const slug = params.slug || 'index'
-    const article = await $content(slug)
-      .fetch()
-      .catch((err) => {
+    const page = await $content(slug)
+      .fetch().catch((err) => {
         error({ statusCode: 404, message: 'Page not found' + err })
       })
 
     return {
-      article
+      page
     }
   },
   head () {
     return {
-      title: this.article.title,
+      title: this.page.title,
       meta: [
-        { hid: 'description', name: 'description', content: this.article.description },
+        { hid: 'description', name: 'description', content: this.page.description },
         // Open Graph
-        { hid: 'og:title', property: 'og:title', content: this.article.title },
-        { hid: 'og:description', property: 'og:description', content: this.article.description },
+        { hid: 'og:title', property: 'og:title', content: this.page.title },
+        { hid: 'og:description', property: 'og:description', content: this.page.description },
         // Twitter Card
-        { hid: 'twitter:title', name: 'twitter:title', content: this.article.title },
-        { hid: 'twitter:description', name: 'twitter:description', content: this.article.description }
+        { hid: 'twitter:title', name: 'twitter:title', content: this.page.title },
+        { hid: 'twitter:description', name: 'twitter:description', content: this.page.description }
       ]
     }
   }
