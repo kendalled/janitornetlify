9e225cc9ecccef98e1f81122cb16fa95309f7ce2
Kendall Jackson <kendallkj00@gmail.com>
2020-11-02 18:57:59 -0500

    fix: update phone number

--- components/ContactSection.vue
+++ components/ContactSection.vue
@@ -38,8 +38,8 @@
                 >
                   <path d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
-                <a href="tel:+18012681543" title="Call our company" class="ml-3 text-blue-50 hover:underline">
-                  +1 (801)-268-1543
+                <a href="tel:+18015004747" title="Call our company" class="ml-3 text-blue-50 hover:underline">
+                  +1 (801)-500-4747
                 </a>
               </dd>
             </div>
